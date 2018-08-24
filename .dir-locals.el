339c829fa2b0193204478405ac5392950c95f4ee
John Kitchin <jkitchin@andrew.cmu.edu>
2018-08-24 14:26:24 -0400

    update locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,4 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((techela-graders . ("John" "Mingjie" "Noriyuki"))
-	 (techela-rubrics . ("default"))
-	 (techela-types . ("homework" "quiz" "exam-1" "exam-2" "exam-3"))
-	 (techela-admin-box-path . (expand-file-name "~/Box Sync/F18-06623/")))))
+((nil . ((techela-label . "f18-06623"))))
